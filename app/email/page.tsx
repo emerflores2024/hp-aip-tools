73ca5b53c949f149bde43646dda6d0c76a62903b
emerflores2024 <emerson.flores170501@liveusam.edu.sv>
2024-08-17 14:34:41 -0600

    building email template interface and validating copy buttons

--- app/email/page.tsx
+++ app/email/page.tsx
@@ -4,11 +4,12 @@ import { Clipboard } from "flowbite-react"
 import { Label, Select } from "flowbite-react";
 import { useState } from "react";
 import { Button, TextInput } from "flowbite-react";
+import { Tooltip } from "flowbite-react";
 import first_pca from "@/email_templates/first_pca";
 
 export default function Email(this: any) {
     const [selectedSubject, setSelectedSubject] = useState('');
-    const [success, setSuccess] = useState(false);
+    const [tooltip, setTooltip] = useState('Copy to clipboard');
 
     const handleSubjectChange = (event: any) => {
         setSelectedSubject(event.target.value);
@@ -18,17 +19,20 @@ export default function Email(this: any) {
         event.target.value = event.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
     };
 
-    function copyEmail(){
+    function copyEmail() {
         const email_body = first_pca;
-        if(email_body){
-            const blob = new Blob([email_body], { type: 'text/html' });
-            const clipboardItem = new window.ClipboardItem({ 'text/html': blob });
-            navigator.clipboard.write([clipboardItem]).then(
-                () => setSuccess(true),
-            );
+        if (email_body) {
+          const blob = new Blob([email_body], { type: 'text/html' });
+          const clipboardItem = new window.ClipboardItem({ 'text/html': blob });
+          navigator.clipboard.write([clipboardItem]).then(() => {
+            setTooltip("Copied");
+            setTimeout(() => {
+              setTooltip("Copy to clipboard");
+            }, 5000);
+          });
         }
-       
-    }
+      }
+      
 
     return (
         <>
@@ -63,47 +67,71 @@ export default function Email(this: any) {
                     </div>
 
 
-                    <div className="w-full mt-2"><Button className="w-full" onClick={copyEmail} color="blue">Generate email</Button></div>
+                    <div className="w-full mt-2"><Button className="w-full" color="blue">Generate email</Button></div>
 
                 </form>
 
-                <div id="email_body" className="w-full px-0 xl:px-10 md:w-4/5 md:mx-auto">
-                    <div className="w-full h-16 rounded-lg bg-gradient-to-r from-purple-700 to-cyan-600 flex justify-between items-center px-5">
-                        <h1 className="text-lg font-bold text-white">Hello, Emersito</h1>
-                        <div><img className="h-10" src="https://hp-emailgenerator.web.app/static/hp-logo.png"></img></div>
-                    </div>
-                    <div className="w-full mt-5 px-5">
-                        <p className="text-white text-justify">
-                            After our last conversation, we have kept your case opened to give you a follow up and we have tried to contact you back via phone/email, but we have been unsuccessful.
-                        </p>
-                        <br></br>
-                        <p className="text-white text-justify">
-                            If you still need assistance from us or require any further help in any other related matter, please contact us back quoting your case number or if you consider that the issue has been addressed, you can reply back to this email, so we can close your case.
-                        </p>
-                        <br></br>
-                        <p className="text-white text-justify">
-                            I would like to thank you once again for your cooperation and patience throughout this process, and if there is anything else you would like me to cover for you, please contact us back and we will be more than happy to help you. Take care and have a nice day!
-                        </p>
-                        <br></br>
-                        <p className="text-white text-justify">
-                            Here is your case number for future reference: <strong>5151231321</strong>
-                        </p>
-                        <br></br>
-                        <br></br>
-                        <div className="flex flex-row items-center">
-                            <div>
-                                <img className="h-20" src="https://cdn-icons-png.freepik.com/512/7555/7555460.png" alt="" />
+                <div className="flex flex-col w-full px-0 xl:px-10 md:w-4/5 md:mx-auto">
+                    <p className="text-xl text-white font-semibold">Your email template</p>
+                    <div className="w-full flex justify-between items-center mt-7">
+                        <div className="grid w-1/2">
+                            <div className="relative">
+                                <label htmlFor="npm-install" className="sr-only">
+                                    Label
+                                </label>
+                                <input
+                                    id="npm-install"
+                                    type="text"
+                                    className="col-span-6 block w-full rounded-lg border border-gray-300 bg-gray-50 px-2.5 py-4 text-sm text-gray-500 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                                    value="All-In Plan Support: Case Follow-Up"
+                                    disabled
+                                    readOnly
+                                />
+                                <Clipboard.WithIconText valueToCopy="npm install flowbite-react" />
                             </div>
-                            <div className="flex flex-col">
-                                <p className="text-white text-justify">
-                                    <strong>Emerson</strong>
-                                </p>
-                                <p className="text-white text-justify">
-                                    HP 24/7 Pro Support | All-In Plan
-                                </p>
-                                <p className="text-white text-justify">
-                                    +1 888-447-0148
-                                </p>
+                        </div>
+                        <Tooltip content={tooltip}>
+                            <Button onClick={copyEmail} color="blue">Copy email</Button>
+                        </Tooltip>
+                    </div>
+                    <div id="email_body" className="mt-7">
+                        <div className="w-full h-16 rounded-lg bg-gradient-to-r from-purple-700 to-cyan-600 flex justify-between items-center px-5">
+                            <h1 className="text-lg font-bold text-white">Hello, Emersito</h1>
+                            <div><img className="h-10" src="https://hp-emailgenerator.web.app/static/hp-logo.png"></img></div>
+                        </div>
+                        <div className="w-full mt-5 px-5">
+                            <p className="text-white text-justify">
+                                After our last conversation, we have kept your case opened to give you a follow up and we have tried to contact you back via phone/email, but we have been unsuccessful.
+                            </p>
+                            <br></br>
+                            <p className="text-white text-justify">
+                                If you still need assistance from us or require any further help in any other related matter, please contact us back quoting your case number or if you consider that the issue has been addressed, you can reply back to this email, so we can close your case.
+                            </p>
+                            <br></br>
+                            <p className="text-white text-justify">
+                                I would like to thank you once again for your cooperation and patience throughout this process, and if there is anything else you would like me to cover for you, please contact us back and we will be more than happy to help you. Take care and have a nice day!
+                            </p>
+                            <br></br>
+                            <p className="text-white text-justify">
+                                Here is your case number for future reference: <strong>5151231321</strong>
+                            </p>
+                            <br></br>
+                            <br></br>
+                            <div className="flex flex-row items-center">
+                                <div>
+                                    <img className="h-20" src="https://cdn-icons-png.freepik.com/512/7555/7555460.png" alt="" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <p className="text-white text-justify">
+                                        <strong>Emerson</strong>
+                                    </p>
+                                    <p className="text-white text-justify">
+                                        HP 24/7 Pro Support | All-In Plan
+                                    </p>
+                                    <p className="text-white text-justify">
+                                        +1 888-447-0148
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
